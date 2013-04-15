46f029eb85d1cec3ea3b6c9be0341422fc7426bc
Bastian Bittorf <bittorf@bluebottle.com>
2013-04-15 20:04:59 +0200

    wbm: config: proto: olsr: rewrote with some hints from JoW

--- packages/wbm-testbed/files/etc/wbm/protos/olsr.sh
+++ packages/wbm-testbed/files/etc/wbm/protos/olsr.sh
@@ -12,71 +12,61 @@ REAL_INTERFACE=$3
 IPV4=$4
 IPV6=$5
 
-clean () {
+prepare() {
   uci revert olsrd
   rm /etc/config/olsrd
   touch /etc/config/olsrd
-}
 
-prepare () {
-  touch /etc/config/olsrd
-  touch /etc/config/network
+  uci -q add olsrd olsrd
+  uci set olsrd.@olsrd[-1].DebugLevel=0
+  uci set olsrd.@olsrd[-1].ClearScreen=no
+  uci set olsrd.@olsrd[-1].AllowNoInt=yes
+  uci set olsrd.@olsrd[-1].IpVersion=6
+  uci set olsrd.@olsrd[-1].FIBMetric=flat
+  uci set olsrd.@olsrd[-1].Willingness=7
+  uci set olsrd.@olsrd[-1].TcRedundancy=2
+  uci set olsrd.@olsrd[-1].LinkQualityFishEye=1
+  uci set olsrd.@olsrd[-1].LinkQualityAlgorithm=etx_ffeth
+  uci set olsrd.@olsrd[-1].MprCoverage=7
 
-  uci set olsrd.@olsrd[0]=olsrd
-  uci set olsrd.@olsrd[0].DebugLevel=0
-  uci set olsrd.@olsrd[0].ClearScreen=no
-  uci set olsrd.@olsrd[0].AllowNoInt=yes
-  uci set olsrd.@olsrd[0].IpVersion=6
-  uci set olsrd.@olsrd[0].FIBMetric=flat
-  uci set olsrd.@olsrd[0].Willingness=7
-  uci set olsrd.@olsrd[0].TcRedundancy=2
-  uci set olsrd.@olsrd[0].LinkQualityFishEye=1
-  uci set olsrd.@olsrd[0].LinkQualityAlgorithm=etx_ffeth
-  uci set olsrd.@olsrd[0].MprCoverage=7
+  uci -q add olsrd LoadPlugin
+  uci set olsrd.@LoadPlugin[-1]=LoadPlugin
+  uci set olsrd.@LoadPlugin[-1].library=olsrd_arprefresh.so.0.1
 
-  uci set olsrd.@LoadPlugin[0]=LoadPlugin
-  uci set olsrd.@LoadPlugin[0].library=olsrd_arprefresh.so.0.1
-  uci set olsrd.@LoadPlugin[1]=LoadPlugin
+  uci -q add olsrd LoadPlugin
+  uci set olsrd.@LoadPlugin[-1]=LoadPlugin
+  uci set olsrd.@LoadPlugin[-1].library=olsrd_jsoninfo.so.0.0
+  uci set olsrd.@LoadPlugin[-1].accept=0.0.0.0
+  uci set olsrd.@LoadPlugin[-1].port=9090
 
-  uci set olsrd.@LoadPlugin[1].library=olsrd_txtinfo.so.0.1
-  uci set olsrd.@LoadPlugin[1].accept=0.0.0.0
-  uci set olsrd.@LoadPlugin[1].port=2006
+  uci -q add olsrd LoadPlugin
+  uci set olsrd.@LoadPlugin[-1]=LoadPlugin
+  uci set olsrd.@LoadPlugin[-1].library=olsrd_txtinfo.so.0.1
+  uci set olsrd.@LoadPlugin[-1].accept=0.0.0.0
+  uci set olsrd.@LoadPlugin[-1].port=2006
 
   uci commit olsrd
 }
 
-add () {
-  uci set network.${LOGICAL_INTERFACE}=interface
-  uci set network.${LOGICAL_INTERFACE}.ifname=${REAL_INTERFACE}
-  uci set network.${LOGICAL_INTERFACE}.proto=static
-  uci set network.${LOGICAL_INTERFACE}.ip6addr="${IPV6}"
-  uci commit network
-
-  uci set olsrd.@Interface[0]=Interface
-  uci set olsrd.@Interface[0].interface=${LOGICAL_INTERFACE}
-  uci set olsrd.@Interface[0].Ip4Broadcast=255.255.255.255
-  uci set olsrd.@Interface[0].speed=4
+add() {
+  uci -q add olsrd Interface
+  uci set olsrd.@Interface[-1].interface=${LOGICAL_INTERFACE}
+  uci set olsrd.@Interface[-1].Ip4Broadcast=255.255.255.255
+  uci set olsrd.@Interface[-1].speed=5
 
   interface_is_wifi()
   {
-    local iface="$( echo "$1" | cut -d'.' -f1 )"
-    grep "$iface:" /proc/net/wireless
+    local iface="${1%%.*}"
+    grep -q "$iface:" /proc/net/wireless
   }
 
-  interface_is_wifi "${REAL_INTERFACE}" && {
-    uci set olsrd.@Interface[0].Mode=ether
-  }
+  if interface_is_wifi "${REAL_INTERFACE}"; then
+    uci set olsrd.@Interface[-1].Mode=mesh
+  else
+    uci set olsrd.@Interface[-1].Mode=ether
+  fi
 
   uci commit olsrd
-}
-
-stop () {
-  /etc/init.d/olsrd stop
-}
-
-start () {
-  stop
-  /etc/init.d/olsrd start
 }
 
 $ACTION
