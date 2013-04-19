02dbab48554ebc202ac75a5640e37cad773120cb
p4u <p4u@dabax.net>
2013-04-20 01:07:20 +0200

    Add script to check wifi status and reset it if not working (by basti)

--- packages/wbm-testbed/files/etc/uci-defaults/firstboot.sh
+++ packages/wbm-testbed/files/etc/uci-defaults/firstboot.sh
@@ -5,4 +5,6 @@ uci add_list uhttpd.main.listen_http="[::]:80"
 uci commit uhttpd
 lua /usr/bin/wbm-config
 ln -s /etc /www/etc
+echo "iptables -t nat -A POSTROUTING -s 192.168.1.0/24 -j MASQUERADE > /etc/rc.local"
+echo "* * * * * sh /etc/wbm/utils/checkwifi.sh" >> /etc/crontabs/root
 echo "*/10 * * * *	lua /usr/bin/wbm-fwupdate" >> /etc/crontabs/root
