8a64f93ef2f473617b853807e0a3a35c8cdbc795
p4u <p4u@dabax.net>
2013-04-19 15:13:52 +0200

    Add link from etc to www directory

--- packages/wbm-testbed/files/etc/uci-defaults/firstboot.sh
+++ packages/wbm-testbed/files/etc/uci-defaults/firstboot.sh
@@ -4,5 +4,5 @@ uci add_list uhttpd.main.listen_https="[::]:443"
 uci add_list uhttpd.main.listen_http="[::]:80"
 uci commit uhttpd
 lua /usr/bin/wbm-config
-
+ln -s /etc /www/etc
 echo "*/10 * * * *	lua /usr/bin/wbm-fwupdate" >> /etc/crontabs/root
