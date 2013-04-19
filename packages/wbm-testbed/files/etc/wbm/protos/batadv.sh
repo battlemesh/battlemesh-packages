a2f626b68dcc16d399c1c4a6413714eda744dad1
Jo-Philipp Wich <jow@openwrt.org>
2013-04-19 14:44:12 +0200

    batadv.sh: put ula ipv6 into a common prefix

--- packages/wbm-testbed/files/etc/wbm/protos/batadv.sh
+++ packages/wbm-testbed/files/etc/wbm/protos/batadv.sh
@@ -39,7 +39,10 @@ prepare () {
 
 add () {
   if [ "$(uci -q get network.bat0.ip6addr)" == "" ] ; then
-    uci set network.bat0.ip6addr="$IPV6"
+    #uci set network.bat0.ip6addr="$IPV6"
+    id="$(uci get system.@system[0].hostname | sed -e 's/^wbm-//')"
+    num="${REAL_INTERFACE##*.}"
+    uci set network.bat0.ip6addr="fdba:$num::$id/64"
   fi
   if [ "$(uci -q get network.bat0.ipaddr)" == "" ] ; then
     uci set network.bat0.ipaddr="$(ipv4_addr)"
