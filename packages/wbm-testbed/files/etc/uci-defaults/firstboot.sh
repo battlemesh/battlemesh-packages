2477a7868b439a1e9f2415312b51762e23463db3
p4u <p4u@dabax.net>
2013-04-20 10:57:04 +0200

    Fix iptables masquerade line and led on in rc.local

--- packages/wbm-testbed/files/etc/uci-defaults/firstboot.sh
+++ packages/wbm-testbed/files/etc/uci-defaults/firstboot.sh
@@ -5,7 +5,7 @@ uci add_list uhttpd.main.listen_http="[::]:80"
 uci commit uhttpd
 lua /usr/bin/wbm-config
 ln -s /etc /www/etc
-echo "iptables -t nat -A POSTROUTING -s 192.168.1.0/24 -j MASQUERADE > /etc/rc.local"
+echo "iptables -t nat -A POSTROUTING -s 192.168.1.0/24 -j MASQUERADE" > /etc/rc.local
 #echo "* * * * * sh /etc/wbm/utils/checkwifi.sh" >> /etc/crontabs/root
-echo default-on > /sys/class/leds/tp-link\:blue\:qss/trigger
+echo 'default-on > /sys/class/leds/tp-link\:blue\:qss/trigger' >> /etc/rc.local
 echo "*/10 * * * *	lua /usr/bin/wbm-fwupdate" >> /etc/crontabs/root
